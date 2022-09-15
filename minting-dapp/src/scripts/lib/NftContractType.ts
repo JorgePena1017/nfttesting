84bebeb28a2364f77d5cf1344d830c0e0a4e873c
JorgePena1017 <jorgepenaortiz@gmail.com>
2022-09-15 18:50:23 -0400

    firstcommit

---
+++ minting-dapp/src/scripts/lib/NftContractType.ts
@@ -0,0 +1,7 @@
+// The name below ("GreatChairs") should match the name of your Solidity contract.
+// It can be updated using the following command:
+// yarn rename-contract NEW_CONTRACT_NAME
+// Please DO NOT change it manually!
+import { GreatChairs as NftContractType } from '../../../../smart-contract/typechain/index';
+
+export default NftContractType;
---
+++ smart-contract/config/CollectionConfig.ts
@@ -0,0 +1,35 @@
+import CollectionConfigInterface from '../lib/CollectionConfigInterface';
+import * as Networks from '../lib/Networks';
+import * as Marketplaces from '../lib/Marketplaces';
+import whitelistAddresses from './whitelist.json';
+
+const CollectionConfig: CollectionConfigInterface = {
+  testnet: Networks.hardhatLocal,
+  mainnet: Networks.ethereumMainnet,
+  // The contract name can be updated using the following command:
+  // yarn rename-contract NEW_CONTRACT_NAME
+  // Please DO NOT change it manually!
+  contractName: 'GreatChairs',
+  tokenName: 'My NFT Token',
+  tokenSymbol: 'MNT',
+  hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
+  maxSupply: 10000,
+  whitelistSale: {
+    price: 0.05,
+    maxMintAmountPerTx: 1,
+  },
+  preSale: {
+    price: 0.07,
+    maxMintAmountPerTx: 2,
+  },
+  publicSale: {
+    price: 0.09,
+    maxMintAmountPerTx: 5,
+  },
+  contractAddress: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
+  marketplaceIdentifier: 'my-nft-token',
+  marketplaceConfig: Marketplaces.openSea,
+  whitelistAddresses,
+};
+
+export default CollectionConfig;
